a1d429e89e302c9da0638799475fb7d26369899a
Clemens Gogoljn <service@cylancer.net>
2023-03-17 15:45:02 +0100

    Fix translation texts in the mail...

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => 0,
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '1.0.4',
+    'version' => '1.0.6',
     'constraints' => [
         'depends' => [
             'typo3' => '11.5.0-11.5.99',
@@ -32,5 +32,7 @@ $EM_CONF[$_EXTKEY] = [
 1.0.2 :: Fix the plugin descriptiom
 1.0.3 :: Add typo script auto load
 1.0.4 :: Add a space on the top of the save button / Removes unused files
+1.0.5 :: Allows the news extension 10.0.x
+1.0.5 :: Fix translation texts in the mail... 
 
 // ---- CHANGELOG ---------- */
